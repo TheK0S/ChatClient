62ce2494c622d8dd94858e2952f70b79b7dfc01c
Kos <turchakkonstantin@gmail.com>
2023-07-17 23:35:14 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ChatClient.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void DockPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ ChatClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ChatClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatClient.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Message> Messages { get; set; }
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<Message>();
+            Messages.Add(new Message { Ouner = "Kos", Date = DateTime.Now, Text = "Hello! My name is Kos  jhdksajhlkashdlsahdlksahdjlaksjhdlakshjdalkjshdlkhsajdlaksjhdlakshdlksajhdashjdakshdhdhdhdhdhdddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" });
+            Messages.Add(new Message { Ouner = "Lex", Date = DateTime.Now, Text = "Hello! My name is Lex" });
+        }
+    }
+}
